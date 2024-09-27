08d2011251548f1035b1dbeb654267b66c218db5
Stas Borychevskyi <sborichevskiy74@gmail.com>
2024-09-27 17:09:16 +0300

    make build

---
+++ index.24a57d33.js
@@ -0,0 +1,2 @@
+!function e(t,n){var r=document.createElement("ul");for(var a in n){var l=document.createElement("li");l.textContent=a,"object"==typeof n[a]&&Object.keys(n[a]).length>0&&e(l,n[a]),r.appendChild(l)}t.appendChild(r)}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.24a57d33.js.map
